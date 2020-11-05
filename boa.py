987be2f7373fe622daf729b2e3192b2f4de31b9f
ejolly <eshin.jolly@gmail.com>
2020-11-05 01:04:08 -0500

    Fitzing with install

--- boa.py
+++ boa.py
@@ -7,9 +7,23 @@ from pathlib import Path
 import click
 from subprocess import call
 import yaml
+import shutil
 
 # import subprocess
 # import os
+
+
+def split_conda_pip(deps):
+    currentpip = [e for e in deps if isinstance(e, dict) and "pip" in e.keys()]
+    if len(currentpip):
+        currentpip = currentpip[0]
+        currentpip = currentpip["pip"]
+    everythingelse = [
+        e
+        for e in deps
+        if isinstance(e, str) or (isinstance(e, dict) and "pip" not in e.keys())
+    ]
+    return currentpip, everythingelse
 
 
 @click.group()
@@ -18,28 +32,15 @@ def cli():
 
 
 @cli.command()
-@click.argument("name", required=False)
 @click.argument("pyv", required=False)
-@click.option(
-    "--activate/--no-activate",
-    default=True,
-    help="auto-activate conda environment on creation",
-)
-@click.option(
-    "--install/--no-install",
-    default=True,
-    help="auto-build the environment and save dependencies to ./env",
-)
-def init(name, pyv, activate, install):
+def init(pyv):
     """
-    Create an environment.yml in the local directory with name = [NAME] and python version = [PYV].\n
+    Create an environment.yml in the local directory with name = [NAME] and python version = [PYV]. Also adds boa as a dependency\n
 
     NAME: name of the environment; only specified within the environment.yml; Default 'env'\n
     PYV: what python version to use; Default 'python=3.6'\n
     """
 
-    if name is None:
-        name = "env"
     if pyv is None:
         pyv = "python=3.6"
     env_file = Path("./environment.yml")
@@ -53,12 +54,63 @@ def init(name, pyv, activate, install):
             ],
         }
         with open("environment.yml", "w") as f:
-            _ = yaml.dump(envdict, f, sort_keys=False)
+            _ = yaml.dump(envdict, f, sort_keys=True)
         click.echo("created environment.yml")
-    if install:
+        click.echo("use 'boa install' to build environment (install conda packages)")
+
+
+@cli.command()
+def clean():
+    """
+    Compeletely remove installed packages in env/
+    """
+    try:
+        shutil.rmtree("./env")
+        click.echo("removed all installed packeges in env/")
+        click.echo("use 'boa install' to rebuild environment")
+    except FileNotFoundError as _:  # noqa
+        click.echo("no env/ present")
+
+
+@cli.command()
+def list():
+    """
+    Show packages and versions in environyment.yml
+    """
+    env_file = Path("./environment.yml")
+    if env_file.exists():
+        click.echo("Environment Packages:")
+        with open(str(env_file), "r") as f:
+            envdict = yaml.load(f, Loader=yaml.FullLoader)
+            deps = envdict["dependencies"]
+            for dep in deps:
+                if isinstance(dep, str):
+                    click.echo(dep)
+                elif isinstance(dep, dict) and "pip" in dep.keys():
+                    click.echo("pip-dependencies:")
+                    for ddep in dep.values():
+                        for d in ddep:
+                            click.echo(f"  {d}")
+    else:
+        raise FileNotFoundError("No environment.yml found. Did you run 'boa init'?")
+
+
+@cli.command()
+@click.argument("libraries", nargs=-1, required=False)
+@click.option(
+    "--pip",
+    is_flag=True,
+    default=False,
+    help="Assume install should happen with pip instead of conda",
+)
+def install(libraries, pip):
+    """
+    Install a new conda package by first adding it to environment.yml and then updating the environment.
+    """
+    if len(libraries) == 0:
         if Path("./env").exists():
             click.echo(
-                "env folder is already present. If you want to reinstall packages in environment.yml, delete it then cpm init"
+                "env folder is already present. If you want to rebuild the environment from scratch first use boa clean."
             )
         else:
             call(
@@ -66,65 +118,29 @@ def init(name, pyv, activate, install):
             )
             click.echo("environment packages installed into env/")
         click.echo("You need to manually activate: conda activate ./env")
-    # if activate:
-    #     myenv = os.environ.copy()
-    #     subprocess.Popen('activate.sh', env=myenv)
-    #     # os.system("source activate ./env")
-    #     # call("source activate ./env", shell=True)
-    #     click.echo("activated local environment")
+    else:
+        with open("environment.yml", "r+") as f:
+            envdict = yaml.load(f, Loader=yaml.FullLoader)
 
-
-@cli.command()
-@click.argument("pkgs", nargs=-1)
-@click.option(
-    "--skipinstall",
-    is_flag=True,
-    default=False,
-    help="Just update environment.yml without actually rebuilding the environment",
-)
-@click.option(
-    "--pip",
-    is_flag=True,
-    default=False,
-    help="Assume install should happen with pip instead of conda",
-)
-def install(pkgs, skipinstall, pip):
-    """
-    Install a new conda package by first adding it to environment.yml and then updating the environment.
-    """
-    with open("environment.yml", "r+") as f:
-        envdict = yaml.load(f, Loader=yaml.FullLoader)
+        currentpip, everythingelse = split_conda_pip(envdict["dependencies"])
         if pip:
-            newdeps = {"pip": list(pkgs)}
-            currentpip = [
-                e
-                for e in envdict["dependencies"]
-                if isinstance(e, dict) and "pip" in e.keys()
-            ]
-            everythingelse = [
-                e
-                for e in envdict["dependencies"]
-                if isinstance(e, str) or (isinstance(e, dict) and "pip" not in e.keys())
-            ]
-            if len(currentpip):
-                currentpip = currentpip[0]
-                currentpip.update(newdeps)
-                envdict["dependencies"] = everythingelse.append(currentpip)
-            else:
-                if 'pip' not in envdict['dependencies']:
-                    envdict['dependencies'].append('pip')
-                envdict['dependencies'].append(newdeps)
+            newdeps = {"pip": set(list(libraries) + currentpip)}
+            # Check if pip is explicitly in deps otherwise add it
+            # Nested check cause they could have pip>19, pip==20, etc
+            if not any(["pip" in e for e in everythingelse]):
+                everythingelse.append("pip")
+            envdict["dependencies"] = everythingelse.append(newdeps)
         else:
-            newdeps = {"dependencies": list(pkgs)}
-            envdict["dependencies"].update(newdeps)
-        f.seek(0)
-        _ = yaml.dump(envdict, f, sort_keys=False)
-        f.truncate()
-    if not skipinstall:
-        call(
-            "conda env update --prefix ./env --file environment.yml  --prune",
-            shell=True,
-        )
+            libraries = [e for e in libraries]
+            for e in libraries:
+                if e not in envdict['dependencies']:
+                    envdict['dependencies'].append(e)
+        with open("environment.yml", "w") as f:
+            _ = yaml.dump(envdict, f, sort_keys=True)
+    call(
+        "conda env update --prefix ./env --file environment.yml  --prune",
+        shell=True,
+    )
 
 
 if __name__ == "__main__":
